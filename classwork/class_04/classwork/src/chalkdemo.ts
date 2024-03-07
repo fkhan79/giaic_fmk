bfebe5093cc6767a37bfc2bdcf8593be92f94cef
Faisal Masood Khan <faisal.masood@zoodpay.com>
2024-03-07 23:47:08 +0500

    Presentation 04
    Sample Code and Presentation for Class 4

---
+++ classwork/class_04/classwork/src/chalkdemo.ts
@@ -0,0 +1,2 @@
+import chalk from "chalk";
+console.log(chalk.green("Hello World"));
---
+++ classwork/class_04/classwork/src/inquirerdemo.ts
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+
+
+let name = await inquirer.prompt([
+    {
+        name: "inputName",
+        type: "text",
+        message: "Enter Your Name:"
+    }
+]);
+console.log(`Welcome ${name.inputName}`);
