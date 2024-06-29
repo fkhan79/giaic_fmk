cba40db5844e13c38c5e9506e1e233a3ffa6f7b8
Faisal Masood Khan <faisal.masood@zoodpay.com>
2024-06-30 03:11:35 +0500

    Update Getters.ts

---
+++ classwork/class_oops_get_set/src/Getters.ts
@@ -0,0 +1,30 @@
+class Rectangle {
+    private _width: number;
+    private _height: number;
+  
+    constructor(width: number, height: number) {
+      this._width = width;
+      this._height = height;
+    }
+  
+    // Getter for the width property
+    get width(): number {
+      return this._width;
+    }
+  
+    // Getter for the height property
+    get height(): number {
+      return this._height;
+    }
+  
+    // Getter for the area property
+    get area(): number {
+      return this._width * this._height;
+    }
+  }
+  
+  const rect = new Rectangle(5, 10);
+  console.log(rect.width); // Output: 5
+  console.log(rect.height); // Output: 10
+  console.log(rect.area); // Output: 50
+  
